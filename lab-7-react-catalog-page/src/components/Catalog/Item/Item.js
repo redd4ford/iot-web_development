0e1e07d2aa693b5a86686add4581ec9265b3a5d6
Viktoriia Yehorova <55955298+redd4ford@users.noreply.github.com>
2020-11-21 19:51:27 +0200

    Added files for lab 7

---
+++ lab-7-react-catalog-page/src/components/Catalog/Item/Item.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Item.css";
+import ViewMoreItemButton from "./ViewMoreItemButton/ViewMoreItemButton.js";
+import PurchaseItemButton from "./PurchaseItemButton/PurchaseItemButton.js";
+
+function Item(props) {
+    return(
+        <div className="item" id={props.id}>
+            <div className="item-img">
+                <img src={props.image} alt="DOOR"></img>
+            </div>
+            <div className="item-description">
+                <h2># {props.id}</h2>
+                <p>PRODUCER: {props.producer}</p>
+                <p>COLOR: {props.color}</p>
+                <p>WEIGHT (KG): {props.weight} | HEIGHT (CM): {props.height}</p>
+            </div>
+            <div className="item-detail">
+                <div className="item-price">
+                    <p><strong>PRICE: </strong></p>
+                    <p><strong>{props.price} UAH</strong></p>
+                </div>
+                <div className="item-buttons">
+                    <ViewMoreItemButton />
+                    <PurchaseItemButton />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Item;
